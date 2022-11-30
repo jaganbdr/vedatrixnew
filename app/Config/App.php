490e96761533dc069d790d86bac06f0a20bac4b4
jaganbdr <jagan.mains@gmail.com>
2022-11-30 18:21:41 +0530

    changed baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://www.vedatrix.xyz/vedatrix/public/';
+    public $baseURL = 'https://www.vedatrix.xyz/';
 
     /**
      * --------------------------------------------------------------------------
